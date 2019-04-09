499296f9f86b9cbb529d1d72e9411d0f21246886
Tony Nguyen <tnguyen@etisalat.ae>
2019-04-10 00:44:17 +0400

    Fix issue with trailing /0 in SCP

--- VMWareHypervisorTunnel/Services/VMWareTunnelHandlers/SCPHandler.cs
+++ VMWareHypervisorTunnel/Services/VMWareTunnelHandlers/SCPHandler.cs
@@ -65,6 +65,9 @@ namespace VMWareHypervisorTunnel.Services.VMWareTunnelHandlers
 
         public override void OnEndOfFile()
         {
+            // A 0 is appended on the end causing a mismatching file.
+            cachedFile = cachedFile.Take(cachedFile.Count() - 1).ToList();
+            System.Diagnostics.Debug.WriteLine("Last " + cachedFile.Last());
             _client.UploadFile(scpPath, cachedFile.ToArray(), scpPath);
             DataReceived?.Invoke(this, Encoding.ASCII.GetBytes(scpPath));
             EofReceived?.Invoke(this, EventArgs.Empty);
